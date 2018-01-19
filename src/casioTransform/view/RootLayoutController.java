548c0090a37d9523c316ad395aaa73291d1bad40
Geoff Nicholls <geoff@sewnandquilted.com.au>
2018-01-19 22:35:35 +1100

    Remove unused fxml code from original templace

--- src/casioTransform/view/RootLayoutController.java
+++ src/casioTransform/view/RootLayoutController.java
@@ -53,70 +53,6 @@ public class RootLayoutController {
 		this.mainApp = mainApp;
 	}
 
-	/**
-	 * Creates an empty address book.
-	 */
-	@FXML
-	private void handleNew() {
-		mainApp.getPersonData().clear();
-		mainApp.setPersonFilePath(null);
-	}
-
-	/**
-	 * Opens a FileChooser to let the user select an address book to load.
-	 */
-	@FXML
-	private void handleOpen() {
-		FileChooser fileChooser = new FileChooser();
-
-		// Set extension filter
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
-		fileChooser.getExtensionFilters().add(extFilter);
-
-		// Show save file dialog
-		File file = fileChooser.showOpenDialog(mainApp.getPrimaryStage());
-
-//		if (file != null) {
-//			mainApp.loadPersonDataFromFile(file);
-//		}
-	}
-
-	/**
-	 * Saves the file to the person file that is currently open. If there is no open
-	 * file, the "save as" dialog is shown.
-	 */
-	@FXML
-	private void handleSave() {
-//		File personFile = mainApp.getPersonFilePath();
-//		if (personFile != null) {
-//			mainApp.savePersonDataToFile(personFile);
-//		} else {
-//			handleSaveAs();
-//		}
-	}
-
-	/**
-	 * Opens a FileChooser to let the user select a file to save to.
-	 */
-	@FXML
-	private void handleSaveAs() {
-		FileChooser fileChooser = new FileChooser();
-
-		// Set extension filter
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
-		fileChooser.getExtensionFilters().add(extFilter);
-
-		// Show save file dialog
-		File file = fileChooser.showSaveDialog(mainApp.getPrimaryStage());
-
-//		if (file != null) {
-//			// Make sure it has the correct extension
-//			if (!file.getPath().endsWith(".xml")) {
-//				file = new File(file.getPath() + ".xml");
-//			}
-//			mainApp.savePersonDataToFile(file);
-//		}
-	}
 
 	/**
 	 * Opens an about dialog.
@@ -153,13 +89,6 @@ public class RootLayoutController {
 		zBatchUpdate();
 	}
 
-	/**
-	 * Opens the birthday statistics.
-	 */
-	@FXML
-	private void handleShowBirthdayStatistics() {
-		mainApp.showBirthdayStatistics();
-	}
 
 	@FXML
 	void handleZ001Select(ActionEvent event) {
