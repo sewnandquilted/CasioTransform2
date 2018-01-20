0310b6575f85fc19b8c89474f129f8fe251822b4
Geoff Nicholls <geoff@sewnandquilted.com.au>
2018-01-20 12:04:21 +1100

    Tidy up layout

--- src/casioTransform/MainApp.java
+++ src/casioTransform/MainApp.java
@@ -22,8 +22,6 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
-import casioTransform.model.Person;
-import casioTransform.model.PersonListWrapper;
 import casioTransform.view.RootLayoutController;
 import casioTransform.view.directoryScanController;
 
@@ -33,34 +31,10 @@ public class MainApp extends Application {
 	private BorderPane rootLayout;
 
 	/**
-	 * The data as an observable list of Persons.
-	 */
-	private ObservableList<Person> personData = FXCollections.observableArrayList();
-
-	/**
 	 * Constructor
 	 */
 	public MainApp() {
 
-		// Add some sample data
-		personData.add(new Person("Hans", "Muster"));
-		personData.add(new Person("Ruth", "Mueller"));
-		personData.add(new Person("Heinz", "Kurz"));
-		personData.add(new Person("Cornelia", "Meier"));
-		personData.add(new Person("Werner", "Meyer"));
-		personData.add(new Person("Lydia", "Kunz"));
-		personData.add(new Person("Anna", "Best"));
-		personData.add(new Person("Stefan", "Meier"));
-		personData.add(new Person("Martin", "Mueller"));
-	}
-
-	/**
-	 * Returns the data as an observable list of Persons.
-	 * 
-	 * @return
-	 */
-	public ObservableList<Person> getPersonData() {
-		return personData;
 	}
 
 	@Override
